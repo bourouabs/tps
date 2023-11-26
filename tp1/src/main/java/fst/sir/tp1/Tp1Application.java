d551fa802737cb098c0b5794a2134c7c69175824
gacem <gacemzouhair01@gmail.com>
2023-11-26 12:34:27 +0100

    init by bourass

--- tp1/src/main/java/fst/sir/tp1/Tp1Application.java
+++ tp1/src/main/java/fst/sir/tp1/Tp1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Tp1Application {
 
+	//lolo
 	public static void main(String[] args) {
 		SpringApplication.run(Tp1Application.class, args);
 	}
